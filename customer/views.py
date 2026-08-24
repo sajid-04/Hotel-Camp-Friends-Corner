51d9cfad086dbf016159e9776ce473cadbee9068
sajid-04 <sajidmultani0044@gmail.com>
2026-08-24 15:00:54 +0530

    Prepare Hotel Camp Friends Corner for deployment

---
+++ customer/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout 
+
+
+def home(request):
+    return render(request, 'customer/home.html')
+
+def menu(request):
+    return render(request, 'customer/menu.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match.")
+            return redirect('signup')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return redirect('signup')
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        user.save()
+
+        messages.success(request, "Account created successfully.")
+        return redirect('signin')
+
+    return render(request, 'customer/signup.html')
